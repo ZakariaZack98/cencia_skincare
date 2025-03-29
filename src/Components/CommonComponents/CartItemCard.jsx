2214120855c5c73721d225acbeeb31a8d5195fc2
ZakariaZack98 <sadeemdzakaria@yahoo.com>
2025-03-29 19:22:02 +0600

    cart item rendering and subtotal calc done

--- src/Components/CommonComponents/CartItemCard.jsx
+++ src/Components/CommonComponents/CartItemCard.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { RxCrossCircled } from 'react-icons/rx'
+import { CartContext } from '../../contexts/CartContext'
 
-const CartItemCard = () => {
+const CartItemCard = ({cartData, setCartData, pid, imgUrl, name, price, qty, subtotal}) => {
+  const {_, setCart} = useContext(CartContext);
+
+  const handleQtyChange = (e, pid) => {
+    const newCartData = [...cartData];
+    const targetProduct = newCartData.find(product => product.pid == pid);
+    const idx = newCartData.indexOf(targetProduct);
+    newCartData[idx].qty = e.currentTarget.value;
+    newCartData[idx].subtotal = (targetProduct.price * e.currentTarget.value).toFixed(2);
+    setCartData(newCartData);
+  } 
+
+  const removeCartItem = (pid) => {
+    const newCartData = [...cartData];
+    const updatedCartData = newCartData.filter(product => product.pid !== pid);
+    setCartData(updatedCartData);
+    setCart(updatedCartData.map(product => product.pid))
+  }
+
   return (
-    <div>
-      
+    <div className='flex items-center w-full py-4'>
+      <div className="w-[50%] flex items-center gap-x-2">
+        <picture>
+          <img src={imgUrl} className='w-10 h-10 object-cover object-center'/>
+        </picture>
+        <p>{name}</p>
+      </div>
+      <div className="w-[10%]"><p className='text-end'>{price}$</p></div>
+      <div className="w-[20%] text-center">
+        <select name="qty" id="qty" className='px-4 py-1 border-2 border-black' onChange={(e) => handleQtyChange(e, pid)}>
+          {[1, 2, 3, 4, 5].map(qty => {
+            return <option value={qty}>{qty}</option>
+          })}
+        </select>
+      </div>
+      <div className="w-[20%] flex justify-end items-center text-end gap-x-10">
+        <p>{subtotal}$</p>
+        <span className='me-2 text-3xl text-gray-500 cursor-pointer' onClick={() => removeCartItem(pid)}>
+          <RxCrossCircled/>
+        </span>
+      </div>
     </div>
   )
 }
--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,27 +1,43 @@
 import React, { useContext, useState } from "react";
 import { CartContext } from "../../contexts/CartContext";
 import { getProductData } from "../../lib/fetch";
+import CartItemCard from "../../Components/CommonComponents/CartItemCard";
 
 const Cart = () => {
   const productData = getProductData();
   const { cart, setCart } = useContext(CartContext);
 
   // TODO: using the cart elements (product id) to find the desired product from the productData array
-  const [cartData, setCartData] = useState(
-    cart.map((cartItemID) => productData.find((product) => product.pid == cartItemID))
-  );
+  const fetchedCartProducts =  cart.map((cartItemID) => productData.find((product) => product.pid == cartItemID))
+
+  const [cartData, setCartData] = useState(fetchedCartProducts.map(product => {
+    return {
+      pid: product.pid,
+      imgUrl: product.imgUrl,
+      name: product.name,
+      price: product.preferredPrice,
+      qty: 1,
+      subtotal: product.preferredPrice,
+    }
+  }))
+  
 
   return (
     <div className="cart bg-cover bg-center bg-no-repeat z-0 font-dmSans">
       <div className="clrOverlay w-full h-full py-30 z-10">
-        <div className="w-[95dvw] mx-auto">
+        <div className="w-[95dvw] mx-auto flex flex-col">
           <div className="flex w-full items-stretch gap-x-7">
             <div className="cartList w-[60%] p-4 bg-lightPink text-gray-800 dark:bg-gray-800 dark:text-lightPink font-semibold rounded-2xl ">
               <div className="flex pb-2 border-b-gray-300 border-b-4">
                 <p className="w-[50%] text-xl">Product</p>
-                <p className="w-[10%] text-xl">Price</p>
-                <p className="w-[20%] text-xl">Quantity</p>
-                <p className="w-[20%] text-xl">Subtotal</p>
+                <p className="w-[10%] text-xl text-end">Price</p>
+                <p className="w-[20%] text-xl text-center">Quantity</p>
+                <p className="w-[20%] text-xl ms-3">Subtotal</p>
+              </div>
+              <div className="flex flex-col">
+              {
+                cartData?.map(product => <CartItemCard key={product.pid} cartData={cartData} setCartData={setCartData} pid={product.pid} imgUrl={product.imgUrl} name={product.name} price={product.price} qty={product.qty} subtotal={product.subtotal}/>)
+              }
               </div>
             </div>
             <div className="amountCalculation w-[40%] p-4 bg-lightPink text-gray-800 dark:bg-gray-800 dark:text-lightPink rounded-2xl">
